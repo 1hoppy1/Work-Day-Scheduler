39cece905e38ecd50176bf66b48b61640a4b4817
1hoppy1 <jahopki@hotmail.com>
2020-05-30 12:59:56 -0600

    current day works

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Work Day Scheduler Starter Code
+
+https://1hoppy1.github.io/Work-Day-Scheduler
---
+++ assets/js/script2.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+var day = moment().format("dddd, MMM Do")
+
+$(currentDay).html(day)
+
+
