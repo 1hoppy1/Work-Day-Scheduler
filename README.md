8b2384afd88dab16a3efce2f7178da104d36dc06
1hoppy1 <jahopki@hotmail.com>
2020-05-30 20:52:08 -0600

    html and js playing together

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Work Day Scheduler Starter Code
 
 https://1hoppy1.github.io/Work-Day-Scheduler
+
+
+Disk Image Credit
+Artist: emey87
+Iconset: Trainee Icons (221 icons)
+License: CC Attribution-No Derivative 4.0
+Commercial usage: Allowed
+Readme file: License.txt
--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,11 +1,163 @@
-
-
-
-
-
-
+// Forces current day to show up on jumbotron
 var day = moment().format("dddd, MMM Do")
-
 $(currentDay).html(day)
+
+// Change color of box depending on what time of day it is
+
+var currentHour = moment().format('H')
+console.log(currentHour)
+
+
+function changeBackground9() {   
+    if (currentHour > 9) {
+        $("#am9").addClass("past");      
+    }
+    if (currentHour === 9) {
+        $("#am9").addClass("present");
+    }
+    if (currentHour < 9) {
+        $("#am9").addClass("future");
+    }
+}
+$(document).ready(function () {
+    changeBackground9();
+});
+
+
+function changeBackground10() {   
+    if (currentHour > 10) {
+        $("#am10").addClass("past");      
+    }
+    if (currentHour === 10) {
+        $("#am10").addClass("present");
+    }
+    if (currentHour < 10) {
+        $("#am10").addClass("future");
+    }
+}
+$(document).ready(function () {
+    changeBackground10();
+});
+
+function changeBackground11() {   
+    if (currentHour > 11) {
+        $("#am11").addClass("past");      
+    }
+    if (currentHour === 11) {
+        $("#am11").addClass("present");
+    }
+    if (currentHour < 11) {
+        $("#am11").addClass("future");
+    }
+}
+$(document).ready(function () {
+    changeBackground11();
+});
+
+function changeBackground12() {   
+    if (currentHour > 12) {
+        $("#pm12").addClass("past");      
+    }
+    if (currentHour === 12) {
+        $("#pm12").addClass("present");
+    }
+    if (currentHour < 12) {
+        $("#pm12").addClass("future");
+    }
+}
+$(document).ready(function () {
+    changeBackground12();
+});
+
+function changeBackground1() {   
+    if (currentHour > 13) {
+        $("#pm1").addClass("past");      
+    }
+    if (currentHour === 13) {
+        $("#pm1").addClass("present");
+    }
+    if (currentHour < 13) {
+        $("#pm1").addClass("future");
+    }
+}
+$(document).ready(function () {
+    changeBackground1();
+});
+
+function changeBackground2() {   
+    if (currentHour > 14) {
+        $("#pm2").addClass("past");      
+    }
+    if (currentHour === 14) {
+        $("#pm2").addClass("present");
+    }
+    if (currentHour < 14) {
+        $("#pm2").addClass("future");
+    }
+}
+$(document).ready(function () {
+    changeBackground2();
+});
+
+function changeBackground3() {   
+    if (currentHour > 15) {
+        $("#pm3").addClass("past");      
+    }
+    if (currentHour === 15) {
+        $("#pm3").addClass("present");
+    }
+    if (currentHour < 15) {
+        $("#pm3").addClass("future");
+    }
+}
+$(document).ready(function () {
+    changeBackground3();
+});
+
+function changeBackground4() {   
+    if (currentHour > 16) {
+        $("#pm4").addClass("past");      
+    }
+    if (currentHour === 16) {
+        $("#pm4").addClass("present");
+    }
+    if (currentHour < 16) {
+        $("#pm4").addClass("future");
+    }
+}
+$(document).ready(function () {
+    changeBackground4();
+});
+
+function changeBackground5() {   
+    if (currentHour > 17) {
+        $("#pm5").addClass("past");      
+    }
+    if (currentHour === 17) {
+        $("#pm5").addClass("present");
+    }
+    if (currentHour < 17) {
+        $("#pm5").addClass("future");
+    }
+    console.log(currentHour);
+}
+$(document).ready(function () {
+    changeBackground5();
+});
+
+
+
+
+
+
+
+
+
+
+
+// Else use ‘day’ theme
+       // $("#foo").addClass("emeaday").removeClass("emeanight");
+
+
 
 
